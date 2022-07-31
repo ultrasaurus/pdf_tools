9a75d8623e55397caf30857a2c0cc41f964bdcd3
Sarah Allen <sarah@ultrasaurus.com>
2022-07-31 16:20:01 -0700

    add rust-bert

--- src/bin/words.rs
+++ src/bin/words.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use pdf::file::File;
 use pdf_tools::page_text;
+use rust_bert::pipelines::pos_tagging::POSModel;
 
 #[derive(Parser)]
 struct Cli {
@@ -9,10 +10,26 @@ struct Cli {
     input: String,
 }
 
+fn tag() -> () {
+    //    Set-up model
+    let pos_model = POSModel::new(Default::default()).expect("default POSModel");
+
+    //    Define input
+    let input = ["Bob is arrogant. Maria is confident."];
+
+    //    Run model
+    let output = pos_model.predict(&input);
+    for (pos, pos_tag) in output[0].iter().enumerate() {
+        println!("{} - {:?}", pos, pos_tag);
+    }
+    {}
+}
+
 fn main() {
     let cli = Cli::parse();
     let input = cli.input;
     let file = File::open(&input).expect("failed to read PDF");
+    tag();
     for (page_num, page) in file.pages().enumerate() {
         if let Ok(page) = page {
             println!("=== PAGE {} ===\n", page_num + 1);
