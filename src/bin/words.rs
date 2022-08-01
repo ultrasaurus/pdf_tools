e402618840f634e33169e1b903c51212fabe12ff
Sarah Allen <sarah@ultrasaurus.com>
2022-07-31 17:41:06 -0700

    call tag() with PDF text

--- src/bin/words.rs
+++ src/bin/words.rs
@@ -12,12 +12,12 @@ struct Cli {
 
 // rust-bert seems to use Parts of Speech tagging labels, that may be this:
 // https://cs.nyu.edu/~grishman/jet/guide/PennPOS.html
-fn tag() -> () {
+fn tag(input_str: &str) -> () {
     //    Set-up model
     let pos_model = POSModel::new(Default::default()).expect("default POSModel");
 
-    //    Define input
-    let input = ["Bob is arrogant. Maria is confident."];
+    //let input = ["Bob is arrogant. Maria is confident."];
+    let input = [input_str];
 
     //    Run model
     let output = pos_model.predict(&input);
@@ -31,12 +31,12 @@ fn main() {
     let cli = Cli::parse();
     let input = cli.input;
     let file = File::open(&input).expect("failed to read PDF");
-    tag();
     for (page_num, page) in file.pages().enumerate() {
         if let Ok(page) = page {
             println!("=== PAGE {} ===\n", page_num + 1);
             if let Ok(text) = page_text(&page, &file) {
                 println!("{}", text);
+                tag(&text);
             } else {
                 println!("ERROR");
             }
